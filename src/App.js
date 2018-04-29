b23b2dc75c6a8199e9355e1845d778cd61273ecd
jmirav <jmira420@gmail.com>
2018-04-29 16:22:18 -0500

    Implements filter on posts

--- src/App.js
+++ src/App.js
@@ -4,18 +4,53 @@ import posts from './posts'
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
+
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: posts,
+      newSearch: ''
+    }
+
+    // this.filterPosts = this.filterPosts.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event) {
+    var updatedList = this.state.list;
+    updatedList = updatedList.filter(function(el) {
+      return el.title.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+    });
+
+    this.setState({
+      newSearch: event.target.value,
+      list: updatedList
+    })
+  }
+
+  // filterPosts(query) {
+  //   return posts.filter(function(el) {
+  //     return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+  //   })
+  // }
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" value={this.state.newSearch} onChange={this.handleChange} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {this.state.list.map((post, index) =>
+            <li key={index}>
+              <a href={post.url}><img src={post.image} /></a>
+              <p>{ post.title }</p>
+            </li>
+          )}
+
         </ul>
       </div>
     )
@@ -24,5 +59,3 @@ class App extends Component {
 
 
 export default App
-
-
