801081a7e0db82985f5f5606f71e2e5a34378502
jmirav <jmira420@gmail.com>
2018-04-29 16:40:50 -0500

    Adjusting code to clear filter

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
 import React, { Component } from 'react';
 import posts from './posts'
 
-
-// Modifica el componente App para implmentar la funcionalidad requerida
-
-
-
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
-      list: posts,
       newSearch: ''
     }
-
-    // this.filterPosts = this.filterPosts.bind(this);
     this.handleChange = this.handleChange.bind(this);
-
+    this.filterPosts = this.filterPosts.bind(this);
   }
 
   handleChange(event) {
-    var updatedList = this.state.list;
-    updatedList = updatedList.filter(function(el) {
-      return el.title.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
-    });
-
     this.setState({
       newSearch: event.target.value,
-      list: updatedList
     })
   }
 
-  // filterPosts(query) {
-  //   return posts.filter(function(el) {
-  //     return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
-  //   })
-  // }
+  filterPosts(post) {
+  	return post.title.toLowerCase().includes(this.state.newSearch.toLowerCase())
+  }
+
+  renderPost(post) {
+    return (
+      <li key={post.title}>
+        <a href={post.url}><img src={post.image} /></a>
+        <p>{post.title}</p>
+      </li>
+    )
+  }
+
   render() {
     return (
       <div>
@@ -44,18 +37,11 @@ class App extends Component {
           <input type="text" placeholder="Ingresa el término de búsqueda" value={this.state.newSearch} onChange={this.handleChange} />
         </div>
         <ul>
-          {this.state.list.map((post, index) =>
-            <li key={index}>
-              <a href={post.url}><img src={post.image} /></a>
-              <p>{ post.title }</p>
-            </li>
-          )}
-
+          {posts.filter(this.filterPosts.bind(this)).map(this.renderPost)}
         </ul>
       </div>
     )
   }
 }
-
 
 export default App
